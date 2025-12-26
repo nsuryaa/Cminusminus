4e9d638258d5043d9a63da571c0e3962ba41735a
Surya <114294036+nsuryaa@users.noreply.github.com>
2025-12-26 14:52:19 +0530

    Add files via upload

---
+++ Code/261225_1408.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> vintFourNumbers = {1, 2, 3, 4};
+
+    for(auto aIterator = vintFourNumbers.begin(); aIterator != vintFourNumbers.end(); ++aIterator)
+    {
+        cout << *aIterator << " ";
+    }
+
+    return 0;
+}
