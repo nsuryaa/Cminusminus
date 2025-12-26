a391d4a1bc913947956b1a22f9753f904dd988ce
Surya <114294036+nsuryaa@users.noreply.github.com>
2025-12-26 17:11:30 +0530

    Add files via upload

---
+++ Code/261225_1517.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned short usAge = 34;
+
+    cout << "Age : " << usAge << endl;
+
+    /*
+    unsigned short - keyword
+    usaAge - identifier
+    34 - constant
+    "Age :" - string
+    << - operator
+    return - keyword
+    0 - constant
+    */
+    return 0;
+}
