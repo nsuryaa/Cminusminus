e3c46203213df3ca8acd6031b2fed528aa6c792d
Surya <114294036+nsuryaa@users.noreply.github.com>
2025-12-26 15:17:03 +0530

    Add files via upload

---
+++ Code/261225_1452.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/*Pass By value*/
+void addTenToNumberValue(unsigned short number)
+{
+    number = number + 10;
+}
+
+/*Pass By Reference*/
+void addTenToNumberReference(unsigned short &number)
+{
+    number = number + 10;
+}
+
+int main()
+{
+    unsigned short myNumber = 5;
+
+    addTenToNumberReference(myNumber);
+    cout << "Value after call by value: " << myNumber << endl;
+
+    return 0;
+}
